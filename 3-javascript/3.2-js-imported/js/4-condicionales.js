0a3a08dcf351e3885bd27d53ba2ccf9113ae1fdf
CesarTun <ctuncetina@gmail.com>
2023-04-19 21:32:56 -0600

    feat: add anotations to the file

---
+++ 3-javascript/3.2-js-imported/js/4-condicionales.js
@@ -0,0 +1,46 @@
+console.log('============conditionals===========');
+
+/*Los condicionales son bifurcaciones en la logica de nuestro codigo */
+
+// 1. ver video champions
+// 2. ver video miranda
+// 3. desplegar menu
+
+// let video = 'CHAMPIONS';
+// let clickInMenu = true;
+
+// if (clickInMenu == true) {
+//     console.log('mostrando menu...');
+// }
+
+// if (video == 'CHAMPIONS') {
+//     console.log('ver video miranda');
+// } else if (video == 'MIRANDA') {
+//     console.log('ver video miranda');
+// } else {
+//     console.log('video no existente');
+// }
+
+// let isOldMayor = '1'; //0: No soy // 1: Si soy
+// if (isOldMayor === 1) {
+//     console.log('Puedo ver esta pagina');
+// } else {
+//     console.log('Mentiroso...');
+// }
+
+// funcion de una lamp
+
+// let lamp = {
+//     isPlugged: true,
+//     lifeFocus: true,
+// }
+
+// if (lamp.isPlugged) {
+//     if (lamp.lifeFocus) {
+//         console.log('Replace the focus');
+//     } else {
+//         console.log('Buy a new lamp');
+//     }
+// } else {
+//     console.log('Plug the lamp');
+// }
