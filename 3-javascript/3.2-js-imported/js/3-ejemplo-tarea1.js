71af2c21f37ab120b0ca06cc86fdabe1c7b6794a
CesarTun <ctuncetina@gmail.com>
2023-04-19 19:31:45 -0600

    feat: add exercices for kata

---
+++ 3-javascript/3.2-js-imported/js/3-ejemplo-tarea1.js
@@ -0,0 +1,10 @@
+const celular = {
+    brand: 'Xiaomi',
+    price: 3500,
+    hasStock: true,
+    company: {name: 'Xiaomi inc', address: {street: 'callesita no.4'}},
+}
+console.log(celular.company.address.street);
+
+let arr = ['Monday', 'Tuesday', 'Wednesday'];
+console.log(arr);
