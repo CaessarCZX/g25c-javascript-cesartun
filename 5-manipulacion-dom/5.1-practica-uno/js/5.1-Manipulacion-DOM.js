fce965dcc0786313963e80be896b95367dcc4cf8
CesarTun <ctuncetina@gmail.com>
2023-04-26 20:09:34 -0600

    feat: add new topic about DOM in html

---
+++ 5-manipulacion-dom/5.1-practica-uno/js/5.1-Manipulacion-DOM.js
@@ -0,0 +1,14 @@
+// Primeros pasos con DOM
+
+// Cambio de contenido a un elemnto
+document.getElementById('titulo').innerHTML = 'Este es otro texto';
+
+//Cambio de estilo a un elemnto
+document.getElementById('buttonExample').style.backgroundColor = 'red';
+document.getElementById('buttonExample').innerHTML = 'This is another text';
+
+// Agregar un elemento a la pagina
+const nuevoParrafo = document.createElement('p');
+nuevoParrafo.setAttribute('id', 'miNuevoParrafo');
+nuevoParrafo.innerHTML = 'Este es mi parrafo nuevo';
+document.body.appendChild(nuevoParrafo);
